813b955e6bd5171c26a522dde73f75ae761b6100
gbbonacini <gbbonacini@users.noreply.github.com>
2015-07-13 18:14:02 +0200

    more database files and script manager

---
+++ database/00.bulletin-all.sql
@@ -0,0 +1,6 @@
+@@ 01.bulletin-drop-table.sql
+@@ 02.bulletin-create-table.sql
+@@ 03.bulletin-alter-table.sql
+--@@ 04.bulletin-create-index.sql
+--@@ 05.bulleitn-drop-sequence.sql
+--@@ 06.bulletin-create-sequence.sql
