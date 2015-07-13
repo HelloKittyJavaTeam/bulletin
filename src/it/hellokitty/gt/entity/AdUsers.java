f97476f152ee007ea3c747bad00c0204325e2a35
lspecchio <lspecchio@gmail.com>
2015-07-13 09:48:11 +0200

    fantasy

--- src/it/hellokitty/gt/entity/AdUsers.java
+++ src/it/hellokitty/gt/entity/AdUsers.java
@@ -20,6 +20,7 @@ import javax.persistence.TemporalType;
  * 
  */
 @Entity
+
 @Table(name="AD_USERS")
 @NamedQuery(name="AdUsers.findAll", query="SELECT x FROM AdUsers x")
 public class AdUsers implements Serializable {
